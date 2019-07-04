cacef3b339479dcb12d9c21e442e9e1e0e8f7e3a
이성진 <chlskatnr1@naver.com>
2019-07-04 20:05:36 +0900

    FIX CSS, header, index

--- header.php
+++ header.php
@@ -15,8 +15,34 @@
                 <button id="btn2" class="btn">로그아웃</button>
             </div>
             <div class="header_logo">
-                <a href="#"><img src="/img/color_requin.png" alt="LOGO_img"></a>
+                <a href="#"><img src="/img/title_logo.png" alt="LOGO_img"></a>
             </div>
         </header>
     </div>
+    <div id="login_frame">
+        <div id="login">
+            <form>
+                <p id="login_title">LOGIN</p>
+                <input type="text" id="login_id" placeholder="ID" class="login_input">
+                <input type="password" id="login_pw" placeholder="Password" class="login_input">
+                <button onclick="login()">login</button>
+                <p id="login_register"><a href="#" onclick="show_signup()">회원가입</a></p>
+            </form>
+        </div>
+    </div>
+    <div id="signup_frame">
+        <div id="signup">
+            <form>
+                <p id="signup_title">Signup</p>
+                <input type="text" placeholder="Nickname" class="signup_input">
+                <button class="overlap_check">중복 체크</button>
+                <input type="text" placeholder="ID" class="signup_input">
+                <button class="overlap_check">중복 체크</button>
+                <input type="password" placeholder="Password" class="signup_input">
+                <input type="password" placeholder="Confirm Password" class="signup_input">
+                <button class="signup_btn">Signup</button>
+            </form>
+        </div>
+    </div>
+    <div id="login_shadow" onclick="hide_login(); hide_signup()"></div>
 </body>
--- index.php
+++ index.php
@@ -10,31 +10,6 @@ include("header.php");
         </div>
     </div>
 </div>
-
-<div id="login_frame">
-    <div id="login">
-        <form>
-            <p id="login_title">LOGIN</p>
-            <input type="text" id="login_id" placeholder="ID" class="login_input">
-            <input type="password" id="login_pw" placeholder="Password" class="login_input">
-            <button onclick="login()">login</button>
-            <p id="login_register"><a href="#" onclick="show_signup()">회원가입</a></p>
-        </form>
-    </div>
-</div>
-<div id="signup_frame">
-    <div id="signup">
-        <form>
-            <p id="signup_title">Signup</p>
-            <input type="text" placeholder="Nickname" class="signup_input">
-            <input type="text" placeholder="ID" class="signup_input">
-            <input type="password" placeholder="Password" class="signup_input">
-            <input type="password" placeholder="Confirm Password" class="signup_input">
-            <button>Signup</button>
-        </form>
-    </div>
-</div>
-<div id="login_shadow" onclick="hide_login(); hide_signup()"></div>
 <?php
 include("footer.php");
 ?>
