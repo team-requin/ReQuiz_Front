a9aa10ca8c794812f0bc8e4af24e91a747a8c687
LeagueLugas <blackblack9685@gmail.com>
2019-07-04 19:30:03 +0900

    Change div to form

--- index.php
+++ index.php
@@ -13,25 +13,25 @@ include("header.php");
 
 <div id="login_frame">
     <div id="login">
-        <div>
+        <form>
             <p id="login_title">LOGIN</p>
             <input type="text" placeholder="ID" class="login_input">
             <input type="password" placeholder="Password" class="login_input">
             <button>login</button>
             <p id="login_register"><a href="#" onclick="show_signup()">회원가입</a></p>
-        </div>
+        </form>
     </div>
 </div>
 <div id="signup_frame">
     <div id="signup">
-        <div>
+        <form>
             <p id="signup_title">Signup</p>
             <input type="text" placeholder="Nickname" class="signup_input">
             <input type="text" placeholder="ID" class="signup_input">
             <input type="password" placeholder="Password" class="signup_input">
             <input type="password" placeholder="Confirm Password" class="signup_input">
             <button>Signup</button>
-        </div>
+        </form>
     </div>
 </div>
 <div id="login_shadow" onclick="hide_login(); hide_signup()"></div>
