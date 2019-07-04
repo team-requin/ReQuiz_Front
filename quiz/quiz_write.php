1a8363e6812aa5d7cb881f58a01bd1a2b324afab
이성진 <chlskatnr1@naver.com>
2019-07-04 22:12:31 +0900

    css, quiz_write

--- quiz/quiz_write.php
+++ quiz/quiz_write.php
@@ -2,7 +2,15 @@
 include("header.php");
 ?>
 <div id="quiz_write_frame">
-    
+    <div id="quiz_write">
+        <div id="quiz_num">
+            <p>1</p>
+        </div>
+        <div id="quiz_problem">
+            <input type="text" id="quiz_question">
+            <input type="text" id="quiz_answer">
+        </div>
+    </div>
 </div>
 <?php
 include("footer.php");
