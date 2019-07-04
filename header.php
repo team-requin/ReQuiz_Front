16500f9969bf32e0c02af85c9e9ee8ec7aa8472c
이성진 <chlskatnr1@naver.com>
2019-07-04 09:26:07 +0900

    ADD
    ALL

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Requiz</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <div id="header">
+        <header>
+            <div class="header_btns">
+                <button id="btn1" class="btn">로그인</button>
+                <button id="btn2" class="btn">로그아웃</button>
+            </div>
+            <div class="header_logo">
+                <a href="#"><img src="img/title_logo.png" alt="LOGO_img"></a>
+            </div>
+        </header>
+    </div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include("header.php");
+?>
+<div id="login">
+        <div>
+            <p id="login_title">LOGIN</p>
+            <input type="text" placeholder="login" class="login_input">
+            <input type="password" placeholder="password" class="login_input">
+            <button>login</button>
+            <p id="login_register"><a href="#">회원가입</a></p>
+        </div>
+    </div>
+    <div id="login_frame"></div>
+<?php
+include("footer.php");
+?>
