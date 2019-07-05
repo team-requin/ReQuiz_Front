6cb46c2bf0c19f76d06a7e7d9e258d01bfd08503
이성진 <chlskatnr1@naver.com>
2019-07-05 22:45:58 +0900

    ADD css, quiz_read

--- quiz/quiz_read.php
+++ quiz/quiz_read.php
@@ -1,6 +1,19 @@
 <?php
 include("header.php");
 ?>
+<div class="quiz_read_frame">
+    <div class="quiz_read">
+        <div class="quiz_read_wapper">
+            <ul class="quiz_ul">
+                <li class="quiz_read_question">
+                    question
+                </li>
+                    answer
+                <li class="quiz_read_answer"></li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 <?php
 include("footer.php");
