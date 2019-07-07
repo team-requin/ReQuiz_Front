86c89e40e730383256d2e0d162658187529604b0
이성진 <chlskatnr1@naver.com>
2019-07-07 22:30:46 +0900

    ADD css to read, list

--- quiz/quiz_list.php
+++ quiz/quiz_list.php
@@ -8,27 +8,28 @@ include("header.php");
     <div id="quiz_profile">
         <img src="http://placehold.it/200x200" alt="profile">
         <div id="quiz_profile_input">
-            <i>asd</i>
+            <p class="profile_user_account">Please enter your account</p>
+            <p class="profile_user_nickname">Please enter your Nickname</p>
             <button>수정</button>
         </div>
     </div>
     <div id="quiz_list">
-        <div>
-            <div>제목</div><button>수정</button>
+        <div class="quiz_list_frame">
+            <div class="quiz_list_title">제목</div>
+            <button class="quiz_list_modify">수정</button>
         </div>
-        <div>
-            <div>제목</div><button>수정</button>
+        <div class="quiz_list_frame">
+            <div class="quiz_list_title">제목</div>
+            <button class="quiz_list_modify">수정</button>
         </div>
-        <div>
-            <div>제목</div><button>수정</button>
+        <div class="quiz_list_frame">
+            <div class="quiz_list_title">제목</div>
+            <button class="quiz_list_modify">수정</button>
         </div>
-        <div>
-            <div>제목</div><button>수정</button>
+        <div class="quiz_list_frame">
+            <div class="quiz_list_title">제목</div>
+            <button class="quiz_list_modify">수정</button>
         </div>
-        <div>
-            <div>제목</div><button>수정</button>
-        </div>
-        <button class="quiz_add_button">+플러스</button>
     </div>
 </div>
 
--- quiz/quiz_read.php
+++ quiz/quiz_read.php
@@ -4,9 +4,9 @@ include("header.php");
 <div class="quiz_read_frame">
     <div class="quiz_read">
         <div class="quiz_read_wapper">
-            <ul class="quiz_ul">
-                <li class="quiz_read_question">question</li>
-                <li class="quiz_read_answer">answer</li>
+            <ul class="quiz_read_ul">
+                <li class="quiz_read_question">Requin은 무엇일까요?</li>
+                <li class="quiz_read_answer">Requin은 대덕소프트웨어마이스터고등학교 5기 학생들이 만든 팀입니다!</li>
             </ul>
             <button class="quiz_more_button">더보기</button>
         </div>
