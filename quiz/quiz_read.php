eefca763c7b77fe2c0864b858204c1547600773e
이성진 <chlskatnr1@naver.com>
2019-07-07 20:51:21 +0900

    Fix css, read

--- quiz/quiz_read.php
+++ quiz/quiz_read.php
@@ -5,12 +5,18 @@ include("header.php");
     <div class="quiz_read">
         <div class="quiz_read_wapper">
             <ul class="quiz_ul">
-                <li class="quiz_read_question">
-                    question
-                </li>
-                    answer
-                <li class="quiz_read_answer"></li>
+                <li class="quiz_read_question">question</li>
+                <li class="quiz_read_answer">answer</li>
+                <li class="quiz_read_question">question</li>
+                <li class="quiz_read_answer">answer</li>
+                <li class="quiz_read_question">question</li>
+                <li class="quiz_read_answer">answer</li>
+                <li class="quiz_read_question">question</li>
+                <li class="quiz_read_answer">answer</li>
+                <li class="quiz_read_question">question</li>
+                <li class="quiz_read_answer">answer</li>
             </ul>
+            <button class="quiz_more_button">더보기</button>
         </div>
     </div>
 </div>
