ffd272bfd0ecdf4b64a553db4b8601de1473690f
이성진 <chlskatnr1@naver.com>
2019-07-08 14:44:32 +0900

    ADD css, solve

--- quiz/quiz_read.php
+++ quiz/quiz_read.php
@@ -3,10 +3,13 @@ include("header.php");
 ?>
 <div class="quiz_read_frame">
     <div class="quiz_read">
-        <div id="quiz_read_wapper">
+        <div id="quiz_read_wrapper">
             <ul class="quiz_read_ul">
                 <li class="quiz_read_question">Requin은 무엇일까요?</li>
                 <li class="quiz_read_answer">Requin은 대덕소프트웨어마이스터고등학교 5기 학생들이 만든 팀입니다!</li>
+                <ul>
+                    <li>asd</li>
+                </ul>
             </ul>
             <ul class="quiz_read_ul">
                 <li class="quiz_read_question">Requin은 무엇일까요?</li>
--- quiz/quiz_solve.php
+++ quiz/quiz_solve.php
@@ -1,6 +1,29 @@
 <?php
 include("header.php");
 ?>
+<div id="quiz_solve">
+    <div class="quiz_solve_wrapper">
+        <div class="quiz_solve_workbook_title">
+            <span>문제집</span>
+        </div>
+
+        <div class="quiz_solve_content_wrapper">
+            <div class="solve_content_title">
+                <div class="quiz_solve_num">문제1</div>
+                <div class="quiz_solve_title">문제 제목</div>
+            </div>
+            <div class="solve_content_answer">
+                <div class="quiz_solve_answer">답</div>
+            </div>
+        </div>
+        
+        <div class="quiz_solve_bottom">
+            <button class="quiz_solve_button" id="quiz_solve_stop">stop</button>
+            <button class="quiz_solve_button" id="quiz_before_stop">before</button>
+            <button class="quiz_solve_button" id="quiz_next_stop">next</button>
+        </div>
+    </div>
+</div>
 
 <?php
 include("footer.php");
