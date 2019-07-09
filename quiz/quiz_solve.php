89610abc222d87c6b140c313e500714be7102d39
이성진 <chlskatnr1@naver.com>
2019-07-09 22:48:33 +0900

    CSS, solve

--- quiz/quiz_solve.php
+++ quiz/quiz_solve.php
@@ -6,13 +6,14 @@ include("header.php");
         <!-- <div class="quiz_solve_workbook_title">
             <span>문제집</span>
         </div> -->
+        <button class="quiz_solve_button" id="quiz_solve_stop">stop</button>
         <div class="quiz_solve_content_wrapper">
             <div class="quiz_solve_content_question">
-                <div class="quiz_solve_num">문제1</div>
+                <div class="quiz_solve_num">Q</div>
                 <div class="quiz_solve_title">문제 제목</div>
             </div>
             <div class="quiz_solve_content_answer">
-                <div class="quiz_solve_answer">답</div>
+                <!-- <div class="quiz_solve_answer">답</div> -->
                 <div class="quiz_solve_input_frame">
                     <input type="text" class="quiz_solve_input" autofocus="on">
                 </div>
@@ -20,7 +21,6 @@ include("header.php");
         </div>
         
         <div class="quiz_solve_buttons">
-            <button class="quiz_solve_button" id="quiz_solve_stop">stop</button>
             <div>
                 <button class="quiz_solve_button" id="quiz_before_stop">before</button>
                 <button class="quiz_solve_button" id="quiz_next_stop">next</button>
