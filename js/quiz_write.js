e987eca21eb06deeeab6305ba5fa47cd1054bcbd
이성진 <chlskatnr1@naver.com>
2019-07-09 20:52:15 +0900

    ADD css, write, write.js

---
+++ js/quiz_write.js
@@ -0,0 +1,56 @@
+
+
+function plus_quiz_write() {
+    var quiz_write_count = document.getElementsByClassName("quiz_write").length
+    if (quiz_write_count >= 20) {
+        return alert('더 이상 만들 수 없습니다.');
+    }
+    var quiz_write_frame = document.getElementById("quiz_write_frame");
+    var quiz_write = document.createElement('div');
+    var quiz_num = document.createElement('div');
+    var p = document.createElement('p');
+    var quiz_problem = document.createElement('div');
+    var form = document.createElement('form');
+    var input_question = document.createElement('input');
+    var input_answer = document.createElement('input');
+    var button = document.createElement('button');
+
+    input_question.setAttribute('type', 'text');
+    input_question.setAttribute('class', 'quiz_question quiz_problems');
+    input_question.setAttribute('placeholder', 'Please enter a question');
+    
+    input_answer.setAttribute('type', 'text');
+    input_answer.setAttribute('class', 'quiz_answer quiz_problems');
+    input_answer.setAttribute('placeholder', 'Please enter a answer');
+
+    quiz_write.setAttribute('class', 'quiz_write')
+    quiz_num.setAttribute('class', 'quiz_num');
+    p.appendChild(document.createTextNode(quiz_write_count+1));
+    quiz_num.appendChild(p);
+
+    quiz_problem.setAttribute('class', 'quiz_problem');
+    button.setAttribute('class', 'quiz_problem_delete');
+    button.appendChild(document.createTextNode('삭제하기'));
+
+    form.appendChild(input_question);
+    form.appendChild(input_answer);
+    quiz_problem.appendChild(form);
+    quiz_problem.appendChild(button);
+    quiz_write.appendChild(quiz_num);
+    quiz_write.appendChild(quiz_problem);
+    quiz_write_frame.appendChild(quiz_write);
+}
+
+function delete_quiz_write() {
+    var quiz_write = document.getElementsByClassName("quiz_write");
+    quiz_write[0].parentNode.removeChild(quiz_write[0]);
+}
+/* function removeDummy() {
+var elem = document.getElementById('dummy');
+elem.parentNode.removeChild(elem);
+}
+*/
+/*
+var header = document.querySelector("h1");	//제거하고자 하는 엘리먼트
+header.parentNode.removeChild(header);
+*/
--- quiz/quiz_write.php
+++ quiz/quiz_write.php
@@ -1,11 +1,24 @@
 <?php
 include("header.php");
 ?>
-<script src="/js/quiz_write.js"></script>
 <div id="quiz_write_frame">
     <div class="quiz_write_title">
         <input class="quiz_write_title_input" type="text" placeholder="Please enter a workbook title" autofocus="on" />
     </div>
+
+    <div class="quiz_write">
+        <div class="quiz_num">
+            <p>1</p>
+        </div>
+        <div class="quiz_problem">
+            <form>
+                <input type="text" class="quiz_question quiz_problems" placeholder="Please enter a question">
+                <input type="text" class="quiz_answer quiz_problems" placeholder="Please enter a answer">
+            </form>
+            <button class="quiz_problem_delete" onclick="delete_quiz_write()">삭제하기</button>
+        </div>
+    </div>
+    <!--
     <div class="quiz_write">
         <div class="quiz_num">
             <p>1</p>
@@ -17,11 +30,15 @@ include("header.php");
             </form>
         </div>
     </div>
-    <button class="quiz_add_problem">+플러스</button>
+    -->
+</div>
+<div id="quiz_write_bottom">
+    <button class="quiz_add_problem" onclick="plus_quiz_write()">+플러스</button>
     <div class="quiz_make_frame">
         <button class="quiz_make_btn">만들기</button>
     </div>
 </div>
+<script src="/js/quiz_write.js"></script>
 <?php
 include("footer.php");
 ?>
