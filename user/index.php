28ef091bace8c9aa572967b6cf5c1b4662fb19d7
이성진 <chlskatnr1@naver.com>
2019-07-10 23:22:06 +0900

    main.css

--- user/index.php
+++ user/index.php
@@ -7,12 +7,11 @@ include('header.php');
         <div id="mypage_main" class="mypage_content">
             <div id="mypage_profile_wrapper">
                 <p id="mypage_profile_hello">안녕하세요, xxx님!!</p>
-                <img src="/img/user_index_background.png" alt="mypage_background_img">
+                <img id="mypage_profile_image" src="../img/color_requin.png" alt="mypage_background_img">
                 <div id="mypage_exp_wrapper">
-                    <div id="mypage_exp">
-                        <div id="mypage_exp_percent">퍼센트</div>
-                    </div>
+                    <div id="mypage_exp"></div>
                 </div>
+                <div id="mypage_exp_percent">1,000 EXP</div>
             </div>
 
             <div id="mypage_input_wrapper">
