b798bbc7e0dae5a70e2c5f189c7d19c637ed5226
LeagueLugas <blackblack9685@gmail.com>
2019-07-11 10:25:41 +0900

    Fix js

--- js/user_info.js
+++ js/user_info.js
@@ -3,18 +3,24 @@ if(!id) {
 	alert('먼저 로그인하세요');
 	location.href = '/';
 }
-axios.post(server+'/auth/token-access', {}, header_token).then((data) => {
-	var id = data.data.user_id;
-	var name = data.data.user_name;
-	var level = data.data.user_level;
-	var exp = data.data.user_exp;
-	var exper = exp/(level*10)*100;
-}).catch((error) => {
-	console.log(error);
-	alert('로그인 세션이 만료되었습니다');
-	sessionStorage.clear();
-	location.href = '/';
-});
+window.onload = () => {
+	axios.post(server+'/auth/token-access', {}, header_token).then((data) => {
+		var id = data.data.user_id;
+		var name = data.data.user_name;
+		var level = data.data.user_level;
+		var exp = data.data.user_exp;
+		var exper = exp/(level*10)*100;
+		var hello_text = document.getElementById('mypage_profile_hello');
+		hello_text.appendChild(document.createTextNode('안녕하세요, '+name+'님!!'));
+		var id_input = document.getElementById('cant_change_id');
+		id_input.setAttribute('value', id);
+	}).catch((error) => {
+		console.log(error);
+		alert('로그인 세션이 만료되었습니다');
+		sessionStorage.clear();
+		location.href = '/';
+	});
+};
 function back_page() {
 	history.back();
 }
